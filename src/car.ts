015a890594e706eadad90e7684543c928200e2c0
Charles Liu <charles.liu0309@gmail.com>
2022-09-04 01:16:05 -0400

    added car move physics

--- src/car.ts
+++ src/car.ts
@@ -3,6 +3,11 @@ class Car {
   y: number;
   width: number;
   height: number;
+  speed: number;
+  acceleration: number;
+  maxSpeed: number;
+  angle: number;
+  friction: number;
   controls: Controls;
 
   constructor(x: number, y: number, width: number, height: number) {
@@ -10,19 +15,71 @@ class Car {
     this.y = y;
     this.width = width;
     this.height = height;
+
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.angle = 0;
+    this.friction = 0.05;
+
     this.controls = new Controls();
+  }
+
+  update() {
+    this.#move();
+  }
+
+  #move() {
+    // direction
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+    const flip = this.speed > 0 ? 1 : -1;
+    if (this.controls.left) {
+      this.angle += 0.03 * flip;
+    }
+    if (this.controls.right) {
+      this.angle -= 0.03 * flip;
+    }
+
+    // limiting speed
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    // friction
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    // prevents bouncing around
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx: CanvasRenderingContext2D | null) {
     if (!ctx) return;
 
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+
+    ctx.restore();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-if (typeof window !== "undefined") {
-  const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
-  canvas.height = window.innerHeight;
-  canvas.width = 200;
+if (typeof window === "undefined") process.exit();
 
-  const ctx = canvas.getContext("2d");
-  const car = new Car(100, 100, 30, 50);
+const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
+
+animate();
+
+function animate() {
+  car.update();
+  canvas.height = window.innerHeight; // clears canvas
   car.draw(ctx);
+  requestAnimationFrame(animate);
 }
