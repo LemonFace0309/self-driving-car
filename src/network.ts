fed5be8e37911866c1aff2711a95af51924f746f
Charles Liu <charles.liu0309@gmail.com>
2022-10-13 18:10:15 -0400

    added nn class

--- src/network.ts
+++ src/network.ts
@@ -1,3 +1,24 @@
+class NeuralNetwork {
+  levels: Level[];
+
+  constructor(neuronCounts: number[]) {
+    this.levels = [];
+
+    for (let i = 0; i < neuronCounts.length - 1; i++) {
+      this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
+    }
+  }
+
+  static feedForwrd(givenInputs: number[], network: NeuralNetwork) {
+    let outputs = Level.feedForwards(givenInputs, network.levels[0]);
+    for (let i = 1; i < network.levels.length; i++) {
+      outputs = Level.feedForwards(outputs, network.levels[i]);
+    }
+
+    return outputs;
+  }
+}
+
 class Level {
   inputs: number[];
   outputs: number[];
